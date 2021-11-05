6796bfa1fcec7839a41d294d10fd08c638aa3cf0
Jason Ryan <jamariryan89@yahoo.com>
2021-11-05 09:03:50 -0500

    Add project files.

---
+++ Prs_Server/Models/PrsDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prs_Server.Models
+{
+    public class PrsDbContext : DbContext
+    {
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Vendor> Vendors { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Request> Requests { get; set; }
+        public virtual DbSet<Requestline> Requestlines { get; set; }
+
+
+        public PrsDbContext(DbContextOptions<PrsDbContext> options)
+           : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<User>(e =>
+            {
+                e.HasIndex(p => p.Username).IsUnique(true);
+            });
+            builder.Entity<Vendor>(e =>
+            {
+                e.HasIndex(p => p.Code).IsUnique(true);
+            });
+            builder.Entity<Product>(e =>
+            {
+                e.HasIndex(p => p.PartNbr).IsUnique(true);
+            });
+            builder.Entity<Request>(e =>
+            {
+
+            });
+            builder.Entity<Requestline>(e =>
+            {
+
+            });
+        }
+    }
+}
---
+++ Prs_Server/Models/Request.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prs_Server.Models
+{
+    public class Request
+    {
+        public int Id { get; set; }
+
+        [StringLength(80), Required]
+        public string Description { get; set; }
+        
+        [StringLength(80), Required]
+        public string Justification { get; set; }
+
+        [StringLength(80)]
+        public string RejectionReason { get; set; }
+
+        [StringLength(20), Required]
+        public string DeliveryMode { get; set; } = "Pickup";
+
+        [StringLength(10), Required]
+        public string Status { get; set; } = "NEW";
+
+        [Column(TypeName = "decimal(11,2)")]
+        public decimal Total { get; set; }
+
+        public int UserId { get; set; }
+
+        public virtual User User { get; set; }
+
+        public virtual ICollection<Requestline> Requestlines { get; set; }
+
+        public Request () { }
+    }
+}
